2eab7e103e40bbd7ebcf90bd239de591298dcfde
phedayat <phedayat@ucsd.edu>
2022-01-09 15:24:06 -0800

    Fixed small bug in JS

--- src/static/index.js
+++ src/static/index.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', (e) => {
 	document.getElementById('test').innerHTML = 'Some new text I added';
 });
 
-let button = document.getElementsByName('login');
+let button = document.getElementsByName('login')[0];
 button.addEventListener('click', (e) => {
 	document.getElementById('test').innerHTML = 'Changed the text!';
 });
