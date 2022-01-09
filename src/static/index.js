86d9f80d5e8b34505a0c694a4c2160ee364eedf0
phedayat <phedayat@ucsd.edu>
2022-01-09 15:22:40 -0800

    Even more JS testing

--- src/static/index.js
+++ src/static/index.js
@@ -1,3 +1,8 @@
 window.addEventListener('DOMContentLoaded', (e) => {
 	document.getElementById('test').innerHTML = 'Some new text I added';
 });
+
+let button = document.getElementsByName('login');
+button.addEventListener('click', (e) => {
+	document.getElementById('test').innerHTML = 'Changed the text!';
+});
