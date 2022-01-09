04d5fafe7821b6ba33c2f14630207af166499327
phedayat <phedayat@ucsd.edu>
2022-01-09 15:04:00 -0800

    Doing some testing with Jinja

--- src/main.py
+++ src/main.py
@@ -4,4 +4,4 @@ app = Flask(__name__, template_folder="./templates")
 
 @app.route("/")
 def index():
-	return render_template("index.html")
+	return render_template("index.html", inp="An inputted text!")
